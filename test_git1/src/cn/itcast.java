fd245ba30cb04efce28897c2c6bcbc6f7f6f2778
KingDemo <KingDemo@qq.com>
2018-10-16 19:59:06 +0800

    第一次提交

---
+++ test_git1/src/cn/itcast.java
@@ -0,0 +1,22 @@
+package cn;
+
+public class itcast {
+    public static void main(String[] args) {
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+        System.out.println("你好吗???");
+    }
+}
